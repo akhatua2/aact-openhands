cb9d458dfc66a67601026386c26a45e57f8f8084
Arpandeep Khatua <arpandeepk@gmail.com>
2024-12-15 21:35:47 +0400

    Updating the path

--- aact_openhands/tests/test_server.py
+++ aact_openhands/tests/test_server.py
@@ -5,7 +5,7 @@ import os
 
 # Add parent directory to path to import app
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from app import app
+from aact_openhands.app import app
 
 class TestFlaskEndpoints(unittest.TestCase):
     """Test Flask server endpoints"""
